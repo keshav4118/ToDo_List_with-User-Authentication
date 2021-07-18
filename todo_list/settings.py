d019683e00b96b1498505a292b9efcd48258a836
keshav4118 <keshav4118@gmail.com>
2021-07-18 07:32:11 +0530

    Configiration

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['todo-list4118.herokuapp.com','127.0.0.1']
 
 
 # Application definition
