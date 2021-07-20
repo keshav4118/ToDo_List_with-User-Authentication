ad3522e2e6bc432e216aef7b5c9586c0088049e8
Keshav Sharma <56512414+keshav4118@users.noreply.github.com>
2021-07-20 13:22:30 +0530

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'todo_list.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -116,11 +116,11 @@ USE_TZ = True
 
 LOGIN_URL = 'login'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,'static')
-]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+ 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+
